ab9a7825f9910dc8929cfb58eb57302980aaf52e
isfan4ik <isfandiyorprogrammer@gmail.com>
2023-10-24 10:06:54 -0700

    ghdlf

---
+++ homrwork3React/src/pages/Bogl.js
@@ -0,0 +1,5 @@
+const Bogl = () => {
+   return <h1>Bogl art</h1>;
+ };
+ 
+ export default Bogl;
---
+++ homrwork3React/src/pages/index.js
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Bogl";
+import Contact from "./pages/User";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Bogl />} /> 
+          <Route path="contact" element={<User />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
